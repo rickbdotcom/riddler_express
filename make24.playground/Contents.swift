9dceb6d76da79987b6dc207e9912a318584468f2
Rick <rickb@rickb.com>
2020-07-10 10:33:52 -0500

    grouped case

--- make24.playground/Contents.swift
+++ make24.playground/Contents.swift
@@ -49,13 +49,13 @@ enum Op {
 
 indirect
 enum Calc {
-	case op(Double, Op, Calc)
+	case op(Calc, Op, Calc)
 	case x(Double)
 
 	var value: Double {
 		switch self {
-		case let .op(x, op, c):
-			return op.perform(x, c.value)
+		case let .op(a, op, b):
+			return op.perform(a.value, b.value)
 		case let .x(x):
 			return x
 		}
@@ -63,8 +63,8 @@ enum Calc {
 
 	var description: String {
 		switch self {
-		case let .op(x, op, cal):
-			return "(\(x) \(op.description) \(cal.description))"
+		case let .op(a, op, b):
+			return "(\(a.description) \(op.description) \(b.description))"
 		case let .x(x):
 			return "\(x)"
 		}
@@ -80,7 +80,7 @@ enum Calc {
 		} else {
 			let value = ops[0].0
 			let op = ops[0].1
-			return .op(value, op, calculation(from: Array(ops.dropFirst())))
+			return .op(.x(value), op, calculation(from: Array(ops.dropFirst())))
 		}
 	}
 }
@@ -90,9 +90,9 @@ func find_matches(equal: Double, valueOps: [(Double, Op)] = [], with values: [Do
 	if valueOps.count == maxDepth {
 		values.forEach { value in
 			let calc = Calc.calculation(from: valueOps + [(value, .nop)])
-//			if calc.value == equal {
+			if calc.value == equal {
 				calc.output()
-	//		}
+			}
 		}
 	} else {
 		for(i, value) in values.enumerated() {
@@ -105,9 +105,41 @@ func find_matches(equal: Double, valueOps: [(Double, Op)] = [], with values: [Do
 	}
 }
 
+// I couldn't think of a general algorithm to handle this case offhand (a op b) op (b op c) and can't spend any more time on this so I just special case it with this sloppy inelegant code
+func find_grouped_matches(equal: Double, with values: [Double], using ops: [Op], maxDepth: Int) {
+	for (ai, a) in values.enumerated() {
+		for (bi, b) in values.enumerated() {
+			if ai == bi { continue }
+			ops.forEach { abOp in
+				ops.forEach { abcdOp in
+					for (ci, c) in values.enumerated() {
+						if ci == ai || ci == bi { continue }
+						for (di, d) in values.enumerated() {
+							if di == ai || di == bi || di == ci { continue }
+							ops.forEach { cdOp in
+								let calc: Calc = .op(
+									.op(.x(a), abOp, .x(b)),
+									abcdOp,
+									.op(.x(c), cdOp, .x(d))
+								)
+								if calc.value == equal {
+									calc.output()
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
 let values = [2.0, 3.0, 3.0, 4.0]
 let value = 24.0
 
 find_matches(equal: 24, with: values, using: Op.all, maxDepth: 3)
+find_grouped_matches(equal: 24, with: values, using: Op.all, maxDepth: 3)
+
 
 
